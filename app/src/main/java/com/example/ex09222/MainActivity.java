7cdbc7d4ca696bc80158dd6e4cb15c822ba5873d
dy3722 <dy3722@bs.amalnet.k12.il>
2026-01-11 23:15:39 +0200

    end!

--- app/src/main/java/com/example/ex09222/MainActivity.java
+++ app/src/main/java/com/example/ex09222/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.ex09222;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author David Yusupov <dy3722@bs.amalnet.k12.il>
@@ -19,14 +30,32 @@ import androidx.core.view.WindowInsetsCompat;
  * Main Activity
  */
 public class MainActivity extends AppCompatActivity {
-    Intent si;
+    private Intent si;
+    private AlertDialog.Builder adb;
+    private final String[] styles = {"film","restaurant","performance","night trip"};
+    private LinearLayout ll;
+    private TextView tvStyle,tvRE;
+    private final String[] re = {"your smile","favorite food","friends","Albert","Marshmelo"};
+    private final List<String> listOfRE= new ArrayList<>();
 
+    /**
+     * Initializes the activity and sets up the UI components.
+     * <p>
+     * This method sets the content view to activity_main, initializes the Intent for
+     * the credits screen, and links the layout and TextView variables to their XML IDs.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously
+     * being shut down then this Bundle contains the data it most recently supplied.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         si = new Intent(this,CreditsActivity.class);
+        ll = findViewById(R.id.main);
+        tvStyle = findViewById(R.id.tvStyle);
+        tvRE = findViewById(R.id.tvRE);
     }
 
     /**
@@ -63,5 +92,156 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Displays a single-choice dialog to select the entertainment style.
+     * <p>
+     * This method opens a dialog with a list of styles. Upon selecting an item,
+     * the dialog closes immediately, updates the description text, and changes
+     * the background color of the main layout to match the chosen style.
+     *
+     * @param view The view that was clicked to trigger this method.
+     */
+    public void style(View view) {
+        adb = new AlertDialog.Builder(this);
+
+        adb.setCancelable(false);
+        adb.setTitle("Choice of entertainment style");
+        adb.setItems(styles, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which)
+                {
+                    case 0:
+                        ll.setBackgroundColor(Color.RED);
+                        tvStyle.setText("Entertainment style: " + "film"); break;
+                    case 1:
+                        ll.setBackgroundColor(Color.YELLOW);
+                        tvStyle.setText("Entertainment style: " + "restaurant"); break;
+                    case 2:
+                        ll.setBackgroundColor(Color.BLUE);
+                        tvStyle.setText("Entertainment style: " + "performance"); break;
+                    case 3:
+                        ll.setBackgroundColor(Color.GRAY);
+                        tvStyle.setText("Entertainment style: " + "night trip"); break;
+                }
+            }
+        });
+
+        AlertDialog ad = adb.create();
+        ad.show();
+    }
+
+    /**
+     * Displays a multi-choice dialog for selecting refreshments and equipment.
+     * <p>
+     * This method allows users to select multiple items from a list. The selections
+     * are stored in a temporary list, and upon confirmation ('ok'), they are displayed
+     * as a comma-separated string in the TextView. The list is cleared after display.
+     *
+     * @param view The view that was clicked to trigger this method.
+     */
+    public void listRE(View view) {
+        adb = new AlertDialog.Builder(this);
+
+        adb.setCancelable(false);
+        adb.setTitle("Refreshment/Equipment Menu");
+        adb.setMultiChoiceItems(re, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked) listOfRE.add(re[which]);
+                else listOfRE.remove(re[which]);
+            }
+        });
+        adb.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tvRE.setText("Refreshment/Equipment: ");
+                for (String oneRE : listOfRE)
+                {
+                    tvRE.setText(tvRE.getText().toString() + oneRE + ", ");
+                }
+                tvRE.setText(tvRE.getText().toString() + "yourself ;)");
+                listOfRE.clear();
+            }
+        });
+        adb.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog ad = adb.create();
+        ad.show();
+    }
+
+    /**
+     * Displays a dialog containing an input field for the user's name.
+     * <p>
+     * This method dynamically creates an EditText view within the dialog.
+     * When the user confirms, a Toast message is displayed greeting the user
+     * with the name they entered.
+     *
+     * @param view The view that was clicked to trigger this method.
+     */
+    public void nameQ(View view) {
+        adb = new AlertDialog.Builder(this);
+
+        adb.setCancelable(false);
+        adb.setTitle("What is your name?");
+        final EditText et = new EditText(this);
+        et.setHint("Your name:");
+        adb.setView(et);
+        adb.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, "Enjoy your time, " + et.getText().toString() + "!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        adb.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog ad = adb.create();
+        ad.show();
+    }
+
+    /**
+     * Displays a confirmation dialog to reset the planner.
+     * <p>
+     * This method asks the user to confirm the reset action. If confirmed,
+     * it sets the background color back to white and clears the text in the
+     * status TextViews to their initial state.
+     *
+     * @param view The view that was clicked to trigger this method.
+     */
+    public void reset(View view) {
+        adb = new AlertDialog.Builder(this);
+
+        adb.setCancelable(false);
+        adb.setTitle("Reset");
+        adb.setMessage("Are you sure?");
+        adb.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ll.setBackgroundColor(Color.WHITE);
+                tvStyle.setText("Entertainment style:");
+                tvRE.setText("Refreshment/Equipment:");
+            }
+        });
+        adb.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog ad = adb.create();
+        ad.show();
     }
 }
